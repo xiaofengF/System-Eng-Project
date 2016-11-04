b43ff40645a55949199cc149124313d85ea5499c
g2archie <g2archie@gmail.com>
2016-11-04 14:05:45 +0000

    Update project-automated-script.sh

--- project-automated-script.sh
+++ project-automated-script.sh
@@ -3,4 +3,5 @@
 sudo rm -rf /home/ubuntu/server
 git clone https://github.com/sekitk/sc1_list_manager.git /home/ubuntu/server
 cp -f /home/ubuntu/application.conf /home/ubuntu/server/conf/
-cd /home/ubuntu/server/  && play run
+cd /home/ubuntu/server/  && play clean
+play run
